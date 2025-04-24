c573422d3f278a7126ea06ae295f55e9f27dba61
EliasXyl <192986928+EliasXyl@users.noreply.github.com>
2025-04-24 17:38:35 +0300

    Complete sumAll function to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    let sum = 0;
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) || typeof a !== 'number' || typeof b !== 'number' || Array.isArray(a) || Array.isArray(b)){
+        return "ERROR";}
+    else {
+    if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = b; i <= a; i++) {
+            sum += i;
+        }
+    };
+    return sum;
 };
+}
 
 // Do not edit below this line
 module.exports = sumAll;
