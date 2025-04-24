664052a4c5b606b4d526628e3ed89a0f9d479a35
EliasXyl <192986928+EliasXyl@users.noreply.github.com>
2025-04-24 16:26:39 +0300

    Complete removeFromArray function to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    const newArray = [];
+    arr.forEach(element => {
+        if (!itemsToRemove.includes(element)) {
+            newArray.push(element)
+        }
+        
+    });
+    return newArray;
 };
+  
 
 // Do not edit below this line
 module.exports = removeFromArray;
