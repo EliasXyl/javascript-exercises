e233cec3ac88b9a9848eab65d663b11913c6f4ae
EliasXyl <192986928+EliasXyl@users.noreply.github.com>
2025-04-25 16:59:44 +0300

    Complete Celsius-Fahrenheit conversion functions with rounded result to pass test suite

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(c) {
+  let celc = (c - 32) * (5/9);
+  return Math.round(celc * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(f) {
+  let faren = f * (9/5) + 32;
+  return Math.round(faren * 10) / 10;
 };
 
 // Do not edit below this line
