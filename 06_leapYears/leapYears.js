c5cefa2935e61278e90636677233d04f59b7b5b8
EliasXyl <192986928+EliasXyl@users.noreply.github.com>
2025-04-25 16:26:48 +0300

    Complete leapYears function to pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(y) {
+    if (y % 4 == 0 && y % 100 !== 0) {
+        return true;
+    } else if (y % 4 == 0 && y % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
